0c41cf961b2b51d9bd088a7b180e3f3e8ffbe943
CharlesFee <Charles200035@gmail.com>
2018-09-22 11:08:47 -0400

    Works
    Just needs to adjust the values based on needs

---
+++ Magnometer_with_light_code.ino
@@ -0,0 +1,84 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_LSM9DS1.h>
+#include <Adafruit_Sensor.h>  // not used in this demo but required!
+
+// i2c
+Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1();
+
+#define LSM9DS1_SCK A5
+#define LSM9DS1_MISO 12
+#define LSM9DS1_MOSI A4
+#define LSM9DS1_XGCS 6
+#define LSM9DS1_MCS 5
+// You can also use software SPI
+//Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1(LSM9DS1_SCK, LSM9DS1_MISO, LSM9DS1_MOSI, LSM9DS1_XGCS, LSM9DS1_MCS);
+// Or hardware SPI! In this case, only CS pins are passed in
+//Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1(LSM9DS1_XGCS, LSM9DS1_MCS);
+double yStart;
+double xStart;
+double zStart;
+boolean starter;
+
+void setupSensor()
+{
+  
+  // 2.) Set the magnetometer sensitivity
+  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_4GAUSS);
+  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_8GAUSS);
+  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_12GAUSS);
+  lsm.setupMag(lsm.LSM9DS1_MAGGAIN_16GAUSS);
+}
+
+
+void setup() 
+{
+  starter = true;
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  while (!Serial) {
+    delay(1); // will pause Zero, Leonardo, etc until serial console opens
+  }
+  
+  Serial.println("LSM9DS1 data read demo");
+  
+  // Try to initialise and warn if we couldn't detect the chip
+  if (!lsm.begin())
+  {
+    Serial.println("Oops ... unable to initialize the LSM9DS1. Check your wiring!");
+    while (1);
+  }
+  Serial.println("Found LSM9DS1 9DOF");
+
+  // helper to just set the default scaling we want, see above!
+  setupSensor();
+}
+
+void loop() 
+{
+  lsm.read();/* ask it to read in the data */ 
+  /* Get a new sensor event */ 
+  sensors_event_t a, m, g, temp;
+
+  lsm.getEvent(&a, &m, &g, &temp); 
+  if(starter == true){
+    xStart = m.magnetic.x;
+    yStart = m.magnetic.y;
+    zStart = m.magnetic.z;
+    starter = false;
+    }
+
+  Serial.print("Mag X: "); Serial.print(m.magnetic.x);   Serial.print(" gauss");
+  Serial.print("\tY: "); Serial.print(m.magnetic.y);     Serial.print(" gauss");
+  Serial.print("\tZ: "); Serial.print(m.magnetic.z);     Serial.println(" gauss");
+
+  Serial.println();
+  
+  if((m.magnetic.x < (xStart-1)) || (m.magnetic.x > (xStart+1)) || (m.magnetic.y < (yStart-1)) || (m.magnetic.y > (yStart+1)) || (m.magnetic.z < (zStart-1)) || (m.magnetic.z > (zStart+1))){
+    digitalWrite(LED_BUILTIN, HIGH);
+    }
+   else{
+    digitalWrite(LED_BUILTIN, LOW);
+   }
+  
+}
